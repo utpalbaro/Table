fce7b46db048c2134cf8846c3a7f7315a7963c88
utpalb <utpalbaro@gmail.com>
2021-05-09 16:46:19 +0530

    Added electron code

---
+++ main.js
@@ -0,0 +1,74 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Save',
+                click(item, focusedWindow) {
+                    saveItems(focusedWindow);
+                }
+            },
+            {
+                label: 'Open',
+                click(item, focusedWindow) {
+                    let dir = dialog.showOpenDialog(focusedWindow, {
+                        properties: ['openDirectory']
+                    });
+                }
+            }
+        ]
+    }
+];
+
+// Add dev tools if in production env
+if (process.env.NODE_ENV != 'production') {
+    menuTemplate.push({
+        label: 'Developer tools',
+        submenu: [
+            {
+                label: 'Toggle developer tools',
+                accelerator: 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html');
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
